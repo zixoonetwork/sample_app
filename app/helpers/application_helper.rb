7dde8c5c37a93370add6a34836529ac66ce63fe5
Žiga Fajfar <fificek@hotmail.com>
2011-10-28 20:55:30 +0200

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return a title on a per-page basis.
+  def title                             # Documentation comment
+    base_title = "Sample App"           # Method definition
+    if @title.nil?                      # Variable assignment
+      base_title                        # Boolean test for nil
+    else
+      "#{base_title} | #{@title}"       # String interpolation
+    end
+  end
 end
